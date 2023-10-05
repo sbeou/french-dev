68a1365935e038687f3cf6b5191b85c348272d97
sbeou13 <design@walf.com.br>
2023-10-05 15:53:05 -0300

    Update works

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './utils/fontawesome-pro-5.15.4-web/css/all.min.css'
-import './utils/global.scss'
+import './utils/fontawesome-pro-5.15.4-web/css/all.min.css';
+import './utils/global.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './utils/store'
